3ef6f36d61e61d29d4367f1ce23e3e9b584901dc
meowmeowcomputers <ryansimonleon@gmail.com>
2017-05-26 09:44:51 -0500

    Created Scorecard class

--- blackjack.js
+++ blackjack.js
@@ -8,6 +8,13 @@ $(document).ready(function () {
     }
 
     }
+  class ScoreCard {
+    constructor(card) {
+      this.score = score;
+    }
+    scoring(card)
+  }
+
 
   class Hand {
     constructor(cards) {
@@ -27,7 +34,7 @@ $(document).ready(function () {
   }
 
   class Deck {
-    constructor(deck) {
+    constructor() {
       let suits = ['hearts', 'diamonds', 'spades', 'clubs'];
       let faces = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
 
